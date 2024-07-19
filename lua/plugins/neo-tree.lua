d58a9f5a27182bc1fe27c2494460ff171fcc9fce
André Kotake <36243244+andre-kotake@users.noreply.github.com>
2024-07-19 19:37:33 -0300

    Refactor

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,11 +1,10 @@
+local get_icon = require("util").get_icon
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = function(_, opts)
-      local get_icon = require("util").get_icon
-      opts.close_if_last_window = true
-
-      opts.filesystem = {
+    opts = {
+      filesystem = {
         bind_to_cwd = true,
         filtered_items = {
           hide_dotfiles = false,
@@ -13,15 +12,9 @@ return {
             "node_modules",
           },
         },
-        follow_current_file = {
-          enable = true,
-          leave_dirs_open = true,
-        },
-      }
-
-      opts.add_blank_line_at_top = true
-
-      opts.source_selector = {
+      },
+      sources = { "filesystem", "buffers", "git_status" },
+      source_selector = {
         winbar = true,
         content_layout = "center",
         sources = {
@@ -38,7 +31,8 @@ return {
             display_name = get_icon("Git", 1, true) .. "Git",
           },
         },
-      }
-    end,
+      },
+      enable_cursor_hijack = true,
+    },
   },
 }
